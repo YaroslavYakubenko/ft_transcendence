7132e81b9be75784fafcd777bdca93a8b64b753d
Your Name <your.email@example.com>
2026-03-20 21:50:09 +0100

    created server.js

---
+++ judith-backend-website-tryout/srcs/server.js
@@ -0,0 +1,16 @@
+const express = require('express'); // load express library and stores it in variable express
+
+const app = express();				// creates your express application
+const PORT = 3000;					// shall it actually be port 3000?
+
+app.get('/health',(req, res) => {	// when request is sent to URL 'health', run the following function
+	res.json({status:'ok'});		// sends json response ('ok') back to client 
+});
+
+app.get('/', (req, res) => {
+	res.send('Server is running');	// res.send sends plain text, res.json specifically sends json
+});
+
+app.listen(PORT, () => {			// starts the server / start listening for incoming requests, using port 3000
+	console.log(`Server running on http://localhost:${PORT}`);	// prints msg in the terminal 
+});
